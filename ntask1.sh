a8ef28277638839cc498f34c9dbe82fb84085371
root <root@ip-172-31-11-152.ap-south-1.compute.internal>
2024-07-24 13:57:02 +0000

    Pushing the 2 tasks to repo

---
+++ ntask1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Ipaddress of a domain
+
+nslookup guvi.in
+
+#CPU/Memory usage of the server
+
+top
+htop
+free -h
+
+#Connectivity b/w 2nodes
+
+ping guvi.in
+traceroute guvi.in
+
---
+++ ntask2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Using nmap
+nmap -p 9000 guvi.com
